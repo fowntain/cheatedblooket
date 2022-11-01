2f3822b248b0e8c40e6130a1cb77c5cb2490de37
fowntain <dean@skool.gq>
2022-11-01 08:33:37 -0400

    Create run.js

---
+++ run.js
@@ -0,0 +1,15 @@
+var iframe = document.getElementById('frameID'),
+    iframeWin = iframe.contentWindow || iframe,
+    iframeDoc = iframe.contentDocument || iframeWin.document;
+
+window.hello = function () {
+    alert('testing from owner!');
+};
+
+$(iframeDoc).ready(function (event) {
+    iframeDoc.open();
+    iframeDoc.write('iframe here');
+    iframeDoc.write('\<script>alert("testing from iframe!");\<\/script>');
+    iframeDoc.write('\<script>parent.hello();\<\/script>');
+    iframeDoc.close();
+});
