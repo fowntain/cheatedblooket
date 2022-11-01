62c46142f0b1f3ffed61157beab1eaa46ce8ddd4
fowntain <dean@skool.gq>
2022-11-01 08:40:29 -0400

    Update run.js

--- run.js
+++ run.js
@@ -8,7 +8,7 @@ window.hello = function () {
 
 $(iframeDoc).ready(function (event) {
     iframeDoc.open();
-    iframeDoc.write('iframe here');
+    iframeDoc.write('CBlooket V1.02');
     iframeDoc.write('\<script>alert("testing from iframe!");\<\/script>');
     iframeDoc.write('\<script>parent.hello();\<\/script>');
     iframeDoc.close();
